6d969bab491bc03c391411fdb8d1fd95da92625d
Marcelo Ludovico <maludovico@msn.com>
2020-08-23 18:17:23 -0300

    Create category footer and page

--- index.php
+++ index.php
@@ -227,6 +227,16 @@ $app->post('/admin/categories/:idcategory', function($idcategory){
   exit;
 });
 
+$app->get('/category/:idcategory', function($idcategory){
+  $category = new Category();
+  $category->get($idcategory);
+  $page = new Page();
+  $page->setTpl('category', array(
+    "category"=>$category->getValues(),
+    "products"=>[]
+  ));
+});
+
 $app->run();
 
 ?>
--- vendor/mludovico/php-classes/src/Models/Category.php
+++ vendor/mludovico/php-classes/src/Models/Category.php
@@ -23,6 +23,7 @@ class Category extends Model{
     if(count($results) === 0)
       throw new \Exception("Falha ao cadastrar categoria");
     $this->setData($results[0]);
+    Category::updateFile();
   }
 
   public function get($idcategory)
@@ -44,6 +45,20 @@ class Category extends Model{
       "DELETE FROM tb_categories WHERE idcategory = :idcategory", array(
         ":idcategory"=>$this->getidcategory()
       )
+    );
+    Category::updateFile();
+  }
+
+  public function updateFile()
+  {
+    $categories = Category::listAll();
+    $html = [];
+    foreach ($categories as $row) {
+      array_push($html, '<li><a href="/category/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+    }
+    file_put_contents(
+      $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'categories-menu.html',
+      implode('', $html)
     );
   }
 }
